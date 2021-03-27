ae347ba6c5ed297de68a52444147094c65d21789
Elentiya <hornik.daria@gmail.com>
2021-03-28 00:18:27 +0100

    Loads of changes

--- Labo04/GrpcGreeter/GrpcGreeter/Services/GreeterService.cs
+++ Labo04/GrpcGreeter/GrpcGreeter/Services/GreeterService.cs
@@ -17,9 +17,13 @@ namespace GrpcGreeter
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
+
             return Task.FromResult(new HelloReply
             {
-                Message = "Hello " + request.Name
+                Message = "Za gorami, za lasami mieszkala sobie Daria Hornik, a obok Kamil Graczyk. Obaj postanowili pojsc na Informatyke " +
+                "i dostali kolejno numery albumu 246700 i 246994. " +
+                  "Pewnego dnia, byl to 29.03.2021r., wlaczyli sobie gRPC na komputerze: " + request.Name + " i poszli na Rozproszone " +
+                  "Systemy Informatyczne."
             });
         }
     }
